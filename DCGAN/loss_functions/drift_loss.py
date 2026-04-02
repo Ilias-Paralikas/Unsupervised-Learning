ce26a67cd32e0122115f659bb68e94662d520f5c
Ilias-Paralikas <paralikasilias@gmail.com>
2026-04-02 08:30:39 +0100

    training

--- DCGAN/loss_functions/drift_loss.py
+++ DCGAN/loss_functions/drift_loss.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 
 class DriftLoss(nn.Module):
-    def __init__(self, drift_weight=1.0):
+    def __init__(self, weight=1.0):
         super().__init__()
-        self.drift_weight = drift_weight
+        self.drift_weight = weight
 
     def forward(self, x):
         return self.drift_weight * torch.mean((x) ** 2)
